6cd2b8de527f2d1ece94b313abc5a4539c196f95
kamalix <147910814+kamalix@users.noreply.github.com>
2024-02-04 11:15:12 +0330

    deeper in redirect view

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,15 +1,20 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic.base import TemplateView,RedirectView
 from .models import Post
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
+#function based view for show template
+"""
 def indexView(request):
     '''
     a function based view to shoe index page
     '''
     context = {'name':'amani'}
     return render(request,'index.html',context)
+"""
 
 class IndexView(TemplateView):
     """
@@ -23,3 +28,16 @@ class IndexView(TemplateView):
         context['posts']=Post.objects.all()
         return context
 
+'''FBV Redirect
+from django.shortcuts import redirect
+def RedirectToMaktab(request):
+    return redirect('http://maktabkhooneh.com')
+'''
+
+class RedirectToMaktab(RedirectView):
+    url = 'http://maktabkhooneh.com'
+
+    def get_redirect_url(self, *args: Any, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
