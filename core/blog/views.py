c58cdfca099fe88da02ccaf3dda23ad440269e68
kamalix <147910814+kamalix@users.noreply.github.com>
2024-02-04 09:55:03 +0330

    template view & redirect view (blog app)
    https://docs.djangoproject.com/en/3.2/ref/class-based-views/base/#django.views.generic.base.RedirectView

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView,RedirectView
+from .models import Post
 
 # Create your views here.
+
+def indexView(request):
+    '''
+    a function based view to shoe index page
+    '''
+    context = {'name':'amani'}
+    return render(request,'index.html',context)
+
+class IndexView(TemplateView):
+    """
+    a class based view to shoe index page
+    """
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"]="kamal"
+        context['posts']=Post.objects.all()
+        return context
+
