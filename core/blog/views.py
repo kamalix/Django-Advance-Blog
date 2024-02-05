38b86544d64e55acacfd63f8e13a1109cd4b31e0
kamalix <147910814+kamalix@users.noreply.github.com>
2024-02-05 15:18:54 +0330

    create UpdateView for edit posts
    https://docs.djangoproject.com/en/5.0/ref/class-based-views/generic-editing/#django.views.generic.edit.UpdateView

--- core/blog/views.py
+++ core/blog/views.py
@@ -3,7 +3,7 @@ from django.db.models.query import QuerySet
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.base import TemplateView,RedirectView
-from django.views.generic import ListView,DetailView,CreateView,FormView
+from django.views.generic import ListView,DetailView,CreateView,FormView,UpdateView
 from .models import Post
 from django.shortcuts import get_object_or_404
 from .forms import PostForm
@@ -85,3 +85,9 @@ class PostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+class PostEditView(UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/'
+
